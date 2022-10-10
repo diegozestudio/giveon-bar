6ad01d0706b2d36c99805db80c55670db499a6ac
Diego <diegoz.estudio@gmail.com>
2022-10-10 20:07:00 -0300

    ADD: Maped and render foods by categorie

--- pages/menu/index.jsx
+++ pages/menu/index.jsx
@@ -1,17 +1,20 @@
 import Head from "next/head";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import FormAddFood from "../../components/FormAddFood";
 import { getCategories } from "../../controllers/controllers";
 import { getFoods } from "../../firebase/firebase";
 import styles from "../../styles/menu.module.scss";
 
-
 function index() {
-  // useEffect(() => {
-  //   console.log("entré:", getCategories()) 
-  // }, [])
-  
+  const [comidas, setComidas] = useState();
+  const [categorias, setCategorias] = useState();
+
+  useEffect(async () => {
+    setComidas(await getFoods());
+    setCategorias(await getCategories());
+  }, []);
+
   return (
     <>
       <Head>
@@ -23,7 +26,22 @@ function index() {
           <img src="/logo.svg" alt="" />
         </div>
 
-        <FormAddFood/>
+        {/* <FormAddFood/> */}
+
+        <div>Categorias:</div>
+        {categorias?.map((categoria) => {
+          return (
+            <div key={categoria}>
+              <div>{categoria}:</div>
+              {comidas?.map((comida) => {
+                if (comida.categoria === categoria) {
+                  return <div key={comida.id}>{comida.nombre}</div>;
+                }
+              })}
+              <br />
+            </div>
+          );
+        })}
 
         <div>
           <Link href="/">Volver a Home</Link>
