aea053e9d1045f88d699f38f3a357432de844ce8
Diego <diegoz.estudio@gmail.com>
2022-11-26 19:22:48 -0300

    change root directory

---
+++ client/src/pages/menu/index.jsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../../styles/menu.module.scss";
+
+function index() {
+  return (
+    <>
+      <Head>
+        <title>Menú - Giveon Bar</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.contImg}>
+          <img src="/logo.svg" alt="" />
+        </div>
+
+       
+        <div>
+          <Link href="/">Volver a Home</Link>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default index;
--- controllers/controllers.js
+++
@@ -1,8 +0,0 @@
-import { getFoods } from "../firebase/firebase";
-
-export const getCategories = async () => {
-    const comidas = await getFoods()
-    const categorias = comidas.map(comida => comida.categoria)
-    const categoriasSinRepetir = [...new Set(categorias)]
-    return categoriasSinRepetir
-}
